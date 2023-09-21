aa98a06d4cb52389a39ac05b330126cb6c1b779d
Cauê Justen Garbi <garbizada@users.noreply.github.com>
2023-09-21 16:49:52 -0300

    almost finished

--- jogo.js
+++ jogo.js
@@ -1,6 +1,8 @@
 
 var altura = 0              // inicio
 var largura = 0
+var vidas = 1
+var tempo = 0
 
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
@@ -11,8 +13,38 @@ function ajustaTamanhoPalcoJogo(){
 
 ajustaTamanhoPalcoJogo()   // fim codigo para ajuste de tamanho de tela
 
+var conometro = setInterval(function() {
+
+    tempo += 1
+
+    document.getElementById('cronometro').innerHTML = tempo
+
+    if(tempo >= 20) {
+        window.location.href = 'vitoria.html'
+    }
+    
+}, 1000)
+
 
 function posicaoRandomica(){
+
+    if(document.getElementById('mosquito')) {           //criando game over + jgabilidade 
+		document.getElementById('mosquito').remove()
+
+        if (vidas > 3) {
+
+
+            window.location.href = 'fim_de_jogo.html'
+
+        } else{
+
+            document.getElementById('v' + vidas).src = "imagens/imagens/coracao_vazio.png"
+        
+            vidas++
+        }
+	}
+
+
     var posicaoX = Math.floor(Math.random() * largura) - 90  // -90 serve pra impedir que a mosca saia da visão da tela
     var posicaoY = Math.floor(Math.random() * altura) - 90 // randomizando o aparecimento dos mosquitos
 
@@ -27,6 +59,10 @@ function posicaoRandomica(){
     mosquito.style.left = posicaoX + "px"
     mosquito.style.top = posicaoY + "px"
     mosquito.style.position = "absolute"
+    mosquito.id = "mosquito"
+    mosquito.onclick = function() {
+        this.remove()           //this identidifica o elemento Html sendo usado no caso aqui o "mosquito"
+    }
 
 
 
